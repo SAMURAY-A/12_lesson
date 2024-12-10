79b0dbe4945ec3431b923a21d5ff3323541276cd
SAMURAY-A <abdullohaziz559@gmail.com>
2024-12-10 12:50:13 +0500

    first commit

---
+++ src/crud/index.js
@@ -0,0 +1,22 @@
+export function addTodo(newTodo, array) {
+    const newArray = [...array];
+    newArray.push(newTodo);
+    return newArray;
+}
+
+export function deleteTodo(todoId, array) {
+    const result = array.filter(({ id }) => todoId !== id);
+    return result;
+}
+
+// export function deleteTodo(id, list) {
+//     return list.filter((todo) => todo.id !== id);
+// }
+
+export function editTodo(list, updatedTodo) {
+    const result = list.map((todo) =>
+        todo.id === updatedTodo.id ? updatedTodo : todo
+    );
+
+    return result;
+}
